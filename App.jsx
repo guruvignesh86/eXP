2c5c92466236faa9003c817b56d7c0ec384e9112
guruvignesh86 <guruvigneshofficial1@gmail.com>
2025-04-10 14:06:13 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,11 +1,13 @@
 
 import "./index.css"
 import ExpenseTracker from './Expenses/Admin/ExpenseTracker'
+import FinanceTracker from "./Expenses/Admin/FinanceTracker";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Admin from './Expenses/Admin/AdminLogin'
 import { AuthProvider } from './Expenses/Routings/Authentic'
 import ProtectedRoute from './Expenses/Routings/ProtectedRoute'
 import AccessDenied from './Expenses/Routings/AccessDenied'
+import FLog from './Expenses/Admin/FLog'
 function App() {
 
 
@@ -16,12 +18,20 @@ function App() {
 <BrowserRouter>
     <Routes>
     <Route path="/" element={<Admin/>}/>  
+    <Route path="/F" element={<FLog/>}/>  
   
     <Route path="/expad" element={
       
       <ProtectedRoute>
       
       <ExpenseTracker />
+      </ProtectedRoute>
+      } />           
+    <Route path="/Flog" element={
+      
+      <ProtectedRoute>
+      
+      <FinanceTracker />
       </ProtectedRoute>
       } />           
 
